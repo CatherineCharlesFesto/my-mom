abfe67d969d568384bd1c2373c237ffbab3eddc9
CatherineCharlesFesto <Catherinecharlesfesto@gmail.com>
2025-12-05 19:59:01 +0300

    Replace class with Tkinter GUI implementation
    Removed class definition and private methods, added a simple GUI using Tkinter.

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-#Write a program to create a class with following variables and methods - 1. Private variable named privateVar that contains an integer value 2. Create a private function privMeth that prints a message 3. Create a function hello that prints the value of privateVar 4. Create an object for the class and call all the functions.
-class catherine:
-    __privateVar=36
-    def __privMeth(self):
-        print("This is a private method")
-        print(self.__privateVar)
-    def hello(self):
-        print("i am inside a normal method")
-        self.__privMeth()
-object=catherine()
-object.hello()
+from tkinter import*
+window=Tk()
+window.title("Simple GUI")
+window.geometry("300x200")
+window.mainloop()
